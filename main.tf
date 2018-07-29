689fb7e0eb074e6a5ccadd2c5b61d33cb9f92cd6
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-07-29 13:18:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,94 @@
-provider "azurerm" {
-  version = "=1.5.0"
-}
-resource "azurerm_resource_group" "k8s" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
-}
-resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = "${var.cluster_name}"
-  location            = "${azurerm_resource_group.k8s.location}"
-  resource_group_name = "${azurerm_resource_group.k8s.name}"
-  dns_prefix          = "${var.dns_prefix}"
+	provider "azurerm" {
+	  version = "=1.5.0"
+	}
+	resource "azurerm_resource_group" "k8s" {
+	  name     = "${var.resource_group_name}"
+	  location = "${var.location}"
+	}
+	resource "azurerm_kubernetes_cluster" "k8s" {
+	  name                = "${var.cluster_name}"
+	  location            = "${azurerm_resource_group.k8s.location}"
+	  resource_group_name = "${azurerm_resource_group.k8s.name}"
+	  dns_prefix          = "${var.dns_prefix}"
 
-  linux_profile {
-    admin_username = "${var.admin_username}"
+	  linux_profile {
+	    admin_username = "${var.admin_username}"
 
-    ssh_key {
-      key_data = "${file("${var.ssh_public_key}")}"
-    }
-  }
+	    ssh_key {
+	      key_data = "${file("${var.ssh_public_key}")}"
+	    }
+	  }
 
-  agent_pool_profile {
-    name            = "default"
-    count           = "${var.agent_count}"
-    vm_size         = "Standard_F2s"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
-  }
+	  agent_pool_profile {
+	    name            = "default"
+	    count           = "${var.agent_count}"
+	    vm_size         = "Standard_F2s"
+	    os_type         = "Linux"
+	    os_disk_size_gb = 30
+	  }
 
-  service_principal {
-    client_id     = "${var.client_id}"
-    client_secret = "${var.client_secret}"
-  }
+	  service_principal {
+	    client_id     = "${var.client_id}"
+	    client_secret = "${var.client_secret}"
+	  }
 
-  tags {
-    Environment = "Dev"
-  }
-}
+	resource "azurerm_storage_account" "aci-sa" {
+	  name                = "${var.resource_storage_acct}"
+	  resource_group_name = "${var.resource_group_name}"
+	  location            = "${var.location}"
+	  account_tier        = "Standard"
+	  account_replication_type = "LRS"
+	}
+
+	resource "azurerm_storage_share" "aci-share" {
+	  name = "${var.resource_aci-dev-share}"
+
+	  resource_group_name  = "${var.resource_group_name}"
+	  storage_account_name = "${var.resource_storage_acct}"
+
+	  quota = 50
+	}
+
+	resource "azurerm_container_group" "aci-helloworld" {
+	  name                = "${var.resource_aci-hw}"
+	  location            = "${var.location}"
+	  resource_group_name = "${var.resource_group_name}"
+	  ip_address_type     = "public"
+	  dns_name_label      = "${var.resource_dns_aci-label}"
+	  os_type             = "linux"
+
+	  container {
+	    name   = "hw"
+	    image  = "seanmckenna/aci-hellofiles"
+	    cpu    ="0.5"
+	    memory =  "1.5"
+	    port   = "80"
+
+	    environment_variables {
+	      "NODE_ENV" = "dev"
+	    }
+
+	    command = "/bin/bash -c '/path to/myscript.sh'"
+
+	    volume {
+	      name       = "logs"
+	      mount_path = "/aci/logs"
+	      read_only  = false
+	      share_name = "${var.resource_aci-dev-share}"
+
+	      storage_account_name  = "${var.resource_storage_acct.aci-sa.name}"
+	      storage_account_key   = "${var.resource_storage_acct.aci-sa.primary_access_key}"
+	    }
+	  }
+
+	  container {
+	    name   = "sidecar"
+	    image  = "microsoft/aci-tutorial-sidecar"
+	    cpu    = "0.5"
+	    memory = "1.5"
+	  }
+
+	  tags {
+	    environment = "Dev"
+	  }
+	}
