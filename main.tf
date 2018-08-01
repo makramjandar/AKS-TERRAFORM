e209a69fe150dc2ebd7af9cac56e4dd4708e0506
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-01 17:21:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,10 +127,10 @@ resource "null_resource" "provision" {
   }
 
   /**
-            provisioner "local-exec" {
-              command = "echo "$(terraform output kube_config)" > ~/.kube/azurek8s && export KUBECONFIG=~/.kube/azurek8s"
-            } 
-          **/
+              provisioner "local-exec" {
+                command = "echo "$(terraform output kube_config)" > ~/.kube/azurek8s && export KUBECONFIG=~/.kube/azurek8s"
+              } 
+            **/
   provisioner "local-exec" {
     command = "helm init"
   }
@@ -139,12 +139,10 @@ resource "null_resource" "provision" {
     command = "kubectl create -f helm-rbac.yaml"
   }
 
-  resource "null_resource" "wait_for_kubernetes_api" {
-    provisioner "local-exec" {
-      command = <<EOF
+  provisioner "local-exec" {
+    command = <<EOF
             sleep 60
       EOF
-    }
   }
 
   provisioner "local-exec" {
@@ -160,14 +158,14 @@ resource "null_resource" "provision" {
   }
 
   /**
-      provisioner "local-exec" {
-        command = "kubectl create -f azure-load-balancer.yaml"
-      }
+        provisioner "local-exec" {
+          command = "kubectl create -f azure-load-balancer.yaml"
+        }
 
-            provisioner "local-exec" {
-              command = "helm install azure-samples/aks-helloworld --set title=\"AKS Ingress Demo\" --set serviceName=\"ingress-demo\""
-            }
-          **/
+              provisioner "local-exec" {
+                command = "helm install azure-samples/aks-helloworld --set title=\"AKS Ingress Demo\" --set serviceName=\"ingress-demo\""
+              }
+            **/
   provisioner "local-exec" {
     command = "helm install -n hclaks stable/jenkins -f values.yaml --version 0.16.6 --wait"
   }
