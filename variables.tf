e82f1867e9069230841defb398e158960720f212
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-01 17:26:34 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,50 +1,60 @@
 variable "resource_group_name" {
-  default     = "hclaks"
-}
-variable "location" {
-  default     = "westeurope"
+  default = "hclaks"
 }
 
+variable "location" {
+  default = "westeurope"
+}
+
+/**
 variable "client_id" {
-  default     = "68865d62-eae7-4873-ac84-2b3a0bc35344"
+  default     = ""
 }
 
 variable "client_secret" {
-  default     = "3fUpt2Fmvl6RWnLZSJUSSoQfj1hjPatJ"
+  default     = ""
 }
+**/
+variable "client_id" {}
+
+variable "client_secret" {}
 
 variable "cluster_name" {
-  default     = "hclaks"
+  default = "hclaks"
 }
 
 variable "dns_prefix" {
-  default     = "hclaks"
+  default = "hclaks"
 }
 
 variable "ssh_public_key" {
-  default     = "/aks-terraform/id_rsa.pub"
+  default = "/aks-terraform/id_rsa.pub"
 }
+
 variable "admin_username" {
-  default     = "aksadmin"
+  default = "aksadmin"
 }
 
 variable "agent_count" {
-  default     = "3"
+  default = "3"
 }
 
 variable "resource_storage_acct" {
-  default     = "acisa12345"
+  default = "acisa12345"
 }
 
 variable "resource_aci-dev-share" {
-  default     = "aci-dev-share"
+  default = "aci-dev-share"
 }
+
 variable "resource_aci-hw" {
-  default     = "aci-helloworld"
+  default = "aci-helloworld"
 }
+
 variable "resource_dns_aci-label" {
-  default     = "aci-dev-hw"
+  default = "aci-dev-hw"
 }
+
 locals {
   username = "clusterUser_${var.cluster_name}_{$var.cluster_name}"
 }
