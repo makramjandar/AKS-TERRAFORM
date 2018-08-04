858dcbccae6848b571fc1d29216026eff56774ec
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-04 23:28:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_storage_account" "acrstorageacc" {
   name                     = "${var.resource_storage_acct}"
   resource_group_name      = "${azurerm_resource_group.k8s.name}"
   location                 = "${azurerm_resource_group.k8s.location}"
-  account_tier             = "Classic"
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
