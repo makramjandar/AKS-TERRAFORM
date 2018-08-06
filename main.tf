c9ee4cfc8eda16f54f61b911a2e5f2dc535e24d4
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-06 22:41:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location            = "${azurerm_resource_group.k8s.location}"
   resource_group_name = "${azurerm_resource_group.k8s.name}"
   dns_prefix          = "${var.dns_prefix}"
+  kubernetes_version  = "1.10.6"
 
   linux_profile {
     admin_username = "${var.admin_username}"
@@ -20,7 +21,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   agent_pool_profile {
     name            = "default"
     count           = "${var.agent_count}"
-    vm_size         = "Standard_F2s"
+    vm_size         = "${var.azurek8s_sku}"
     os_type         = "Linux"
     os_disk_size_gb = 30
   }
