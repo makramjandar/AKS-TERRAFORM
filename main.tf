bb09b6f622451885881a966c176037329d313503
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-12 10:32:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location            = "${azurerm_resource_group.k8s.location}"
   resource_group_name = "${azurerm_resource_group.k8s.name}"
   dns_prefix          = "${var.dns_prefix}"
-  kubernetes_version  = "1.10.6"
+  kubernetes_version  = "${var.kube_version}"
 
   linux_profile {
     admin_username = "${var.admin_username}"
