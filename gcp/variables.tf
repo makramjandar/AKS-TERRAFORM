7e79ba61f09cec3f4fb3c13d9741343e86143e33
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-25 16:37:00 +0200

    Update variables.tf

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,27 +1,27 @@
-variable "var.cluster_name" {
+variable "cluster_name" {
   description = "Please input the GKE cluster name "
 }
 
-variable "var.cluster_location" {
+variable "cluster_location" {
   description = "Please input the cluster location like us-east1 "
 }
 
-variable "var.node_count" {
+variable "node_count" {
   description = "Please enter the node count "
 }
 
-variable "var.master_auth_username" {
+variable "master_auth_username" {
   description = "Please enter the master auth username "
 }
 
-variable "var.master_auth_password" {
+variable "master_auth_password" {
   description = "Please enter the master auth password "
 }
 
-variable "var.cluster_label" {
+variable "cluster_label" {
   description = "Please enter the cluster label "
 }
 
-variable "var.cluster_tag" {
+variable "cluster_tag" {
   description = "Please enter the cluster tag "
 }
