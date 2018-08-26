ee3f64b30c864592f93546e874fceeae10a64f14
Dwai Banerjee <dwaiba@users.noreply.github.com>
2018-08-26 15:57:06 +0200

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -35,6 +35,57 @@ resource "google_container_cluster" "primary" {
   }
 }
 
+resource "null_resource" "provision" {
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials ${var.cluster_name} --zone ${var.cluster_location}-a --project ${var.project}"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl create serviceaccount -n kube-system tiller && kubectl create clusterrolebinding tiller-binding --clusterrole=cluster-admin --serviceaccount kube-system:tiller"
+  }
+
+  provisioner "local-exec" {
+    command = "curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh"
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 700 get_helm.sh"
+  }
+
+  provisioner "local-exec" {
+    command = "./get_helm.sh"
+  }
+
+  provisioner "local-exec" {
+    command = "helm init --service-account tiller --upgrade"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+            if [ "${var.helm_install_jenkins}" = "true" ]; then
+                helm install -n ${var.cluster_name} stable/jenkins -f ../jenkins-values.yaml --version 0.16.18
+            else
+                echo ${var.helm_install_jenkins}
+            fi
+      EOF
+
+    timeouts {
+      create = "20m"
+      delete = "20m"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+            if [ "${var.patch_svc_lbr_external_ip}" = "true" ]; then
+                kubectl patch svc kubernetes-dashboard -p '{"spec":{"type":"LoadBalancer"}}' --namespace kube-system
+            else
+                echo ${var.patch_svc_lbr_external_ip}
+            fi
+      EOF
+  }
+}
+
 # The following outputs allow authentication and connectivity to the GKE Cluster.
 output "client_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
@@ -47,4 +98,3 @@ output "client_key" {
 output "cluster_ca_certificate" {
   value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
-
