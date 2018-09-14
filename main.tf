1fe3fe9330ca171e0f6f0c9a7398a886a1637fcd
dwaiba <github@cloudgear.io>
2018-09-14 14:14:17 +0200

    Change route logic for Daemonset apply in GPU Skus

--- main.tf
+++ main.tf
@@ -85,10 +85,10 @@ resource "null_resource" "provision" {
   }
 
   /**
-                                                                                        provisioner "local-exec" {
-                                                                                          command = "echo "$(terraform output kube_config)" > ~/.kube/azurek8s && export KUBECONFIG=~/.kube/azurek8s"
-                                                                                        } 
-                                                                                      **/
+                                                                                            provisioner "local-exec" {
+                                                                                              command = "echo "$(terraform output kube_config)" > ~/.kube/azurek8s && export KUBECONFIG=~/.kube/azurek8s"
+                                                                                            } 
+                                                                                          **/
   provisioner "local-exec" {
     command = "helm init --upgrade"
   }
@@ -112,10 +112,10 @@ resource "null_resource" "provision" {
   }
 
   /**
-                                                                          provisioner "local-exec" {
-                                                                            command = "kubectl create -f azure-load-balancer.yaml"
-                                                                          }
-                                                                  **/
+                                                                              provisioner "local-exec" {
+                                                                                command = "kubectl create -f azure-load-balancer.yaml"
+                                                                              }
+                                                                      **/
   provisioner "local-exec" {
     command = "helm repo add azure-samples https://azure-samples.github.io/helm-charts/ && helm repo add gitlab https://charts.gitlab.io/ && helm repo add ibm-charts https://raw.githubusercontent.com/IBM/charts/master/repo/stable/ && helm repo add bitnami https://charts.bitnami.com/bitnami"
   }
@@ -187,24 +187,26 @@ resource "null_resource" "provision" {
 
   provisioner "local-exec" {
     command = <<EOF
-            if [[ "${var.azurek8s_sku}" =~ "NC" ]] && [[ "${var.kube_version}" =~ "1.11" ]]; then
+    skuseries=$(echo ${var.azurek8s_sku}|cut -d'_' -f 2|cut -c1-2)
+    kube_major=$(echo ${var.kube_version}|cut -d'.' -f 1-2)
+    if [ "$skuseries" = "NC" ] && [ "$kube_major" = "1.11" ]; then
               kubectl apply -f nvidia-device-plugin-ds.yaml --namespace kube-system
-            else
-                echo ${var.azurek8s_sku}
-            fi
+    else
+        echo ${var.azurek8s_sku}
+    fi
       EOF
   }
 
   /**
-                                    provisioner "local-exec" {
-                                      command = "cd prometheus-operator && helm install helm/prometheus-operator --name prometheus-operator --namespace monitoring --set rbacEnable=false --wait --timeout 1000"
+                                        provisioner "local-exec" {
+                                          command = "cd prometheus-operator && helm install helm/prometheus-operator --name prometheus-operator --namespace monitoring --set rbacEnable=false --wait --timeout 1000"
 
-                                      timeouts {
-                                        create = "16m"
-                                        delete = "16m"
-                                      }
-                                    }
-                                  **/
+                                          timeouts {
+                                            create = "16m"
+                                            delete = "16m"
+                                          }
+                                        }
+                                      **/
   provisioner "local-exec" {
     command = "cd prometheus-operator && mkdir -p helm/kube-prometheus/charts"
   }
