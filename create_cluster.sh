b794a35f22c7245da29c9187284cd501a57e4260
dwaiba <github@cloudgear.io>
2018-09-17 14:37:41 +0200

    branch script change

--- create_cluster.sh
+++ create_cluster.sh
@@ -18,7 +18,7 @@ exec 1>&2
 echo -e "Please note that docker should be installed local along with the terraform binary and your id_rsa.pub should be present in the present dir. and keep your Azure client_id and secret handy. Happy k8sing !\n"
 read -p "Enter your name or BU Name for aks creation: " yournameorBU
 echo $yournameorBU;
-docker run -dti --name=azure-cli-python-$yournameorBU --restart=always azuresdk/azure-cli-python && docker cp terraform azure-cli-python-$yournameorBU:/root && docker cp id_rsa.pub azure-cli-python-$yournameorBU:/root && docker exec -ti azure-cli-python-$yournameorBU bash -c "az login && cd ~/ && git clone https://github.com/dwaiba/aks-terraform && cd ~/aks-terraform && cp ~/id_rsa.pub ~/aks-terraform && cp ~/terraform /usr/bin && terraform init && terraform plan -out run.plan && terraform apply "run.plan" && bash";
+docker run -dti --name=azure-cli-python-$yournameorBU --restart=always azuresdk/azure-cli-python && docker cp terraform azure-cli-python-$yournameorBU:/root && docker cp id_rsa.pub azure-cli-python-$yournameorBU:/root && docker exec -ti azure-cli-python-$yournameorBU bash -c "az login && cd ~/ && git clone https://github.com/dwaiba/aks-terraform && cd ~/aks-terraform && git checkout aks-terraform-Ubuntu-teslak80-without-RDMA && cp ~/id_rsa.pub ~/aks-terraform && cp ~/terraform /usr/bin && terraform init && terraform plan -out run.plan && terraform apply "run.plan" && bash";
 
 trap - EXIT
 }
