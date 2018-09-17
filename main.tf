286ddc55bb872a3f45cac8d27a97680b50abd756
dwaiba <github@cloudgear.io>
2018-09-17 16:00:58 +0200

    race condition comment change

--- main.tf
+++ main.tf
@@ -85,10 +85,10 @@ resource "null_resource" "provision" {
   }
 
   /**
-                                                                                                                                                              provisioner "local-exec" {
-                                                                                                                                                                command = "echo "$(terraform output kube_config)" > ~/.kube/azurek8s && export KUBECONFIG=~/.kube/azurek8s"
-                                                                                                                                                              } 
-                                                                                                                                                            **/
+                                                                                                                                                                provisioner "local-exec" {
+                                                                                                                                                                  command = "echo "$(terraform output kube_config)" > ~/.kube/azurek8s && export KUBECONFIG=~/.kube/azurek8s"
+                                                                                                                                                                } 
+                                                                                                                                                              **/
   provisioner "local-exec" {
     command = "helm init --upgrade"
   }
@@ -112,10 +112,10 @@ resource "null_resource" "provision" {
   }
 
   /**
-                                                                                                                                                provisioner "local-exec" {
-                                                                                                                                                  command = "kubectl create -f azure-load-balancer.yaml"
-                                                                                                                                                }
-                                                                                                                                        **/
+                                                                                                                                                  provisioner "local-exec" {
+                                                                                                                                                    command = "kubectl create -f azure-load-balancer.yaml"
+                                                                                                                                                  }
+                                                                                                                                          **/
   provisioner "local-exec" {
     command = "helm repo add azure-samples https://azure-samples.github.io/helm-charts/ && helm repo add gitlab https://charts.gitlab.io/ && helm repo add ibm-charts https://raw.githubusercontent.com/IBM/charts/master/repo/stable/ && helm repo add bitnami https://charts.bitnami.com/bitnami"
   }
@@ -182,9 +182,9 @@ resource "null_resource" "kube-prometheus-clone" {
 
 resource "null_resource" "kube-racecheck" {
   /**
-              Kubernetes Race condition check for older than 1.11.2 version- https://github.com/kubernetes/kubernetes/issues/62725
-              This is when CRD status is complete but API Server is not actually available. Kicks in for kune-prometheus for older than 11.1.2 k8s version
-              **/
+                Kubernetes Race condition check for older than 1.11.2 version- https://github.com/kubernetes/kubernetes/issues/62725
+                This is specific when CRD status is complete but API Server is not actually available. Kicks in for kune-prometheus for older than 11.1.2 k8s version
+                **/
   provisioner "local-exec" {
     command = <<EOF
     kube_major=$(echo ${var.kube_version}|cut -d'.' -f 1-2)
